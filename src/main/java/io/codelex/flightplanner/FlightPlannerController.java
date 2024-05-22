a339641b79b74bcc15c072885a597c6b6bfa2489
GatisTC <155078187+GatisTC@users.noreply.github.com>
2024-05-22 12:09:00 +0300

    first part done

---
+++ src/main/java/io/codelex/flightplanner/FlightPlannerController.java
@@ -0,0 +1,68 @@
+package io.codelex.flightplanner;
+
+import io.codelex.flightplanner.domain.Airport;
+import io.codelex.flightplanner.domain.Flight;
+import io.codelex.flightplanner.domain.PageResult;
+import io.codelex.flightplanner.domain.SearchFlightsRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+
+@RestController
+@Validated
+public class FlightPlannerController {
+
+
+    private final FlightPlannerService flightPlannerService;
+
+    public FlightPlannerController(FlightPlannerService flightPlannerService) {
+        this.flightPlannerService = flightPlannerService;
+    }
+
+    @GetMapping("/admin-api/flights/{id}")
+    public Flight adminGetFlight(@PathVariable Long id) {
+        return flightPlannerService.findFlightById(id);
+    }
+
+    @PostMapping("/testing-api/clear")
+    public void clear() {
+        flightPlannerService.clearFlights();
+    }
+
+    @PutMapping("/admin-api/flights")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Flight adminSaveFlight(@Valid @RequestBody Flight flight) {
+        flightPlannerService.saveFlight(flight);
+        return flight;
+    }
+
+    @DeleteMapping("/admin-api/flights/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void adminDeleteFlight(@PathVariable Long id) {
+        flightPlannerService.deleteFlight(id);
+    }
+
+    @GetMapping("/api/airports")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Airport> searchAirports(@RequestParam String search) {
+        return flightPlannerService.getAirportDetails(search);
+    }
+
+    @PostMapping("/api/flights/search")
+    @ResponseStatus(HttpStatus.OK)
+    public PageResult<Flight> searchFlights(@Valid @RequestBody SearchFlightsRequest request) {
+        return flightPlannerService.searchFlights(request);
+    }
+
+    @GetMapping("/api/flights/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Flight findFlightById(@PathVariable Long id) {
+        return flightPlannerService.findFlightById(id);
+    }
+
+}
---
+++ src/main/java/io/codelex/flightplanner/domain/Flight.java
@@ -0,0 +1,107 @@
+package io.codelex.flightplanner.domain;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+
+
+import java.util.Objects;
+
+
+public class Flight {
+    private Long id;
+    @Valid
+    private Airport from;
+    @Valid
+    private Airport to;
+    @NotBlank
+    private String carrier;
+    @NotBlank
+    private String departureTime;
+    @NotBlank
+    private String arrivalTime;
+
+    public Flight() {
+    }
+
+    public Flight(Airport fromAirport, Airport toAirport, String carrier, String departureTime, String arrivalTime) {
+        this.from = fromAirport;
+        this.to = toAirport;
+        this.carrier = carrier;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Airport getFrom() {
+        return from;
+    }
+
+    public void setFrom(Airport from) {
+        this.from = from;
+    }
+
+    public Airport getTo() {
+        return to;
+    }
+
+    public void setTo(Airport to) {
+        this.to = to;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Flight flight)) return false;
+        return Objects.equals(getFrom(), flight.getFrom()) && Objects.equals(getTo(), flight.getTo()) && Objects.equals(getCarrier(), flight.getCarrier()) && Objects.equals(getDepartureTime(), flight.getDepartureTime()) && Objects.equals(getArrivalTime(), flight.getArrivalTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFrom(), getTo(), getCarrier(), getDepartureTime(), getArrivalTime());
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", from=" + from +
+                ", to=" + to +
+                ", carrier='" + carrier + '\'' +
+                ", departureTime=" + departureTime +
+                ", arrivalTime=" + arrivalTime +
+                '}';
+    }
+}
+
+

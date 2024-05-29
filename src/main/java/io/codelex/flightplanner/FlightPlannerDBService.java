0983d522a9ebb11712b7461ef24d27358d894a19
GatisTC <155078187+GatisTC@users.noreply.github.com>
2024-05-29 15:14:39 +0300

    added FlightValidator class

--- src/main/java/io/codelex/flightplanner/FlightPlannerDBService.java
+++ src/main/java/io/codelex/flightplanner/FlightPlannerDBService.java
@@ -4,11 +4,11 @@ import io.codelex.flightplanner.domain.Airport;
 import io.codelex.flightplanner.domain.Flight;
 import io.codelex.flightplanner.domain.PageResult;
 import io.codelex.flightplanner.domain.SearchFlightsRequest;
+import io.codelex.flightplanner.util.FlightValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
 
 public class FlightPlannerDBService implements FlightPlannerService {
 
@@ -24,16 +24,15 @@ public class FlightPlannerDBService implements FlightPlannerService {
 
     public void saveFlight(Flight flight) {
         List<Flight> existingFlights = flightPlannerRepository.getFlights();
-        checkIfDuplicateFlight(flight, existingFlights);
-        checkIfSameAirportToAndFrom(flight);
-        checkIfDatesAreValid(flight);
+        FlightValidator.checkIfDuplicateFlight(flight, existingFlights);
+        FlightValidator.checkIfSameAirportToAndFrom(flight);
+        FlightValidator.checkIfDatesAreValid(flight);
         flightPlannerRepository.saveFlight(flight);
     }
 
     public void deleteFlight(Long id) {
         flightPlannerRepository.deleteFlightById(id);
     }
-
 
     public Flight findFlightById(Long id) {
         Flight flight = flightPlannerRepository.getFlightById(id);
@@ -54,29 +53,4 @@ public class FlightPlannerDBService implements FlightPlannerService {
         List<Flight> flights = flightPlannerRepository.searchFlights(request);
         return new PageResult<>(0L, (long) flights.size(), flights);
     }
-
-
-    private static void checkIfDuplicateFlight(Flight flight, List<Flight> existingFlights) {
-        if (existingFlights.contains(flight)) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Flight already exists.");
-        }
-    }
-
-
-    private static void checkIfSameAirportToAndFrom(Flight flight) {
-        if (flight.getFrom() == null || flight.getTo() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Both departure and arrival airports must be provided.");
-        }
-        if (flight.getFrom().getAirport().trim().equalsIgnoreCase(flight.getTo().getAirport().trim())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Departure and arrival airports cannot be the same.");
-        }
-    }
-
-    private static void checkIfDatesAreValid(Flight flight) {
-        if (flight.getDepartureTime().isAfter(flight.getArrivalTime()) || flight.getDepartureTime().isEqual(flight.getArrivalTime())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Departure time must be before arrival time.");
-        }
-    }
-
-
 }
